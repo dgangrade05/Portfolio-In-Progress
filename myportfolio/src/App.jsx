7f35cd57ab3547ec539a7718d57ca7c56e1a134e
Dhanraj Gangrade <dhanraj.gangrade00@gmail.com>
2023-08-19 18:24:07 +0530

    added notes in index.html

---
+++ myportfolio/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Projects from './components/projects/Projects'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+import Nav from './components/nav/Nav'
+
+const App = () => {
+  return (
+    <>
+    <Home />
+    <Nav />
+    <About />
+    <Experience />
+    <Projects />
+    <Contact />
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ myportfolio/src/components/home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <header>
+      <div className='container home-container'>
+
+        {/* main heading */}
+
+        <h1>
+          Hi, I'm Dhanraj
+        </h1>
+        <h3>
+          A developer in Indore.
+        </h3>
+
+        {/* CARDS  */}
+      </div>
+    </header>
+  )
+}
+
+export default Home
