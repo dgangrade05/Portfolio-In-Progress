561a221bd250181bf66dcdcb06478d194cc3df64
Dhanraj Gangrade <dhanraj.gangrade00@gmail.com>
2023-08-21 09:15:29 +0530

    fixed notes and javascript

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -6,13 +6,11 @@ import Projects from './components/projects/Projects'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Nav from './components/nav/Nav'
-import Notes from './components/notes/Notes'
 
 const App = () => {
   return (
     <>
     <Home />
-    <Notes/>
     <Nav />
     <About />
     <Experience />
--- myportfolio/src/components/notes/Notes.jsx
+++ myportfolio/src/components/notes/Notes.jsx
@@ -1,74 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect } from 'react';
 import './notes.css'
 
-const Notes = () => {
-  const noteColors = [
-    "#b8ffc6",
-    "#faefcc",
-    // Add more colors as needed
-  ];
+function Notes() {
+  let activeNote = null;
+  let offsetX, offsetY;
 
-  const [notes, setNotes] = useState([]);
-  const [activeNote, setActiveNote] = useState(null);
-  const [offsetX, setOffsetX] = useState(0);
-  const [offsetY, setOffsetY] = useState(0);
+  function startDrag(e) {
+    activeNote = e.target;
+    offsetX = e.clientX - activeNote.getBoundingClientRect().left;
+    offsetY = e.clientY - activeNote.getBoundingClientRect().top;
+  }
 
-  const startDrag = (e) => {
-    // setActiveNote(e.target);
-    console.log(e)
-
-    setOffsetX(e.clientX - e.target.getBoundingClientRect().left);
-    setOffsetY(e.clientY - e.target.getBoundingClientRect().top);
-    // window.addEventListener("mousemove", drag);
-    // window.addEventListener("mouseup", stopDrag);
-  };
-
-  const drag = (e) => {
+  function drag(e) {
     if (!activeNote) return;
     activeNote.style.left = e.clientX - offsetX + "px";
     activeNote.style.top = e.clientY - offsetY + "px";
-  };
+  }
 
-  const stopDrag = () => {
-    setActiveNote(null);
-    // window.removeEventListener("mousemove", drag);
-    // window.removeEventListener("mouseup", stopDrag);
-  };
+  function stopDrag() {
+    activeNote = null;
+  }
 
   useEffect(() => {
-    const screenWidth = window.innerWidth;
-    const updatedNotes = [...notes];
+    const notes = document.querySelectorAll(".note");
+    notes.forEach((note) => {
+      note.addEventListener("mousedown", startDrag);
+      note.addEventListener("mousemove", drag);
+      note.addEventListener("mouseup", stopDrag);
 
-    if (screenWidth <= 1024) {
-      updatedNotes[0] = { top: "420px", left: "550px" };
-      updatedNotes[1] = { top: "340px", left: "670px" };
-    } else {
-      updatedNotes[0] = { top: "410px", left: "885px" };
-      updatedNotes[1] = { top: "310px", left: "980px" };
+      note.addEventListener("click", () => {
+        note.classList.add("rotate");
+        setTimeout(() => {
+          note.classList.remove("rotate");
+        }, 1000);
+      });
+    });
+
+    function animateNotesIn() {
+      notes.forEach((note) => {
+        note.classList.add("slide-in");
+      });
     }
 
-    setNotes(updatedNotes);
-  }, []);
+    animateNotesIn();
+
+    window.addEventListener("beforeunload", () => {
+      notes.forEach((note) => {
+        // Perform any necessary cleanup before the component unloads
+      });
+    });
+
+    return () => {
+      notes.forEach((note) => {
+        note.removeEventListener("mousedown", startDrag);
+        note.removeEventListener("mousemove", drag);
+        note.removeEventListener("mouseup", stopDrag);
+      });
+
+      window.removeEventListener("beforeunload", () => {
+        notes.forEach((note) => {
+          // Perform any necessary cleanup before the component unloads
+        });
+      });
+    };
+  },);
 
   return (
     <div>
       <div
-        className="note"
+        className="note" id='note1'
         onDragStart={(e)=>startDrag(e)}
         style={{
-          backgroundColor: noteColors[0],
-          top: notes[0]?.top,
-          left: notes[0]?.left,
+          backgroundColor: "#b8ffc6",
+          top: 420,
+        //   left: 930,
+            right: 520,
         }}
       >
         <p className="notes-p">Open for<br />Intern roles.</p>
       </div>
       <div
-        className="note"
+        className="note" id='note2'
         style={{
-          backgroundColor: noteColors[1],
-          top: notes[1]?.top,
-          left: notes[1]?.left,
+          backgroundColor: "#faefcc",
+          top:330 ,
+        //   left: 1000,
+            right: 420,
         }}
       >
         <p className="hello-icon">👋</p>
