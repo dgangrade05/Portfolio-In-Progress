a82fcdabf0de9deccca0aa6da8ad1f52d362606c
Dhanraj Gangrade <dhanraj.gangrade00@gmail.com>
2023-08-29 19:59:27 +0530

    fixed responsiveness and added a navbar

--- myportfolio/src/components/nav/Nav.jsx
+++ myportfolio/src/components/nav/Nav.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './nav.css'
+import {GoHome} from 'react-icons/go'
+import {SiAboutdotme} from 'react-icons/si'
+import {RxRocket} from 'react-icons/rx'
+import {HiOutlineSparkles} from 'react-icons/hi'
+import {HiOutlineMail} from 'react-icons/hi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#home'id='element' name='home'><GoHome /></a>
+      <a href='#about' id='element' name='about'><SiAboutdotme /></a>
+      <a href='#experience' id='element' name='experience'><RxRocket /></a>
+      <a href='#projects' id='element' name='projects'><HiOutlineSparkles /></a>
+      <a href='#contact' id='element' name='contact'><HiOutlineMail /></a>
+    </nav>
   )
 }
 
