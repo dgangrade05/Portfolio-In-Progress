131b7842973810b73145c90dda13c5ab9a964e68
Dhanraj Gangrade <dhanraj.gangrade00@gmail.com>
2023-09-07 00:21:53 +0530

    added projects section

--- myportfolio/src/components/projects/Projects.jsx
+++ myportfolio/src/components/projects/Projects.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import "./projects.css";
 const Projects = () => {
   return (
-    <section id='projects'>Projects</section>
-  )
-}
+    <section id="projects">
+      <h1 className="projects-heading">PROJECTS</h1>
+      <div className="projects-container">
+        <div className="projects-content">
+          <h2 className="coming-soon">
+            coming soon.
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
